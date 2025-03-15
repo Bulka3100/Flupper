48edf03f9016118d8d69ebfc15fea917736bbe25
Breaker <kirillvip3100@yandex.ru>
2025-03-15 14:55:46 +0300

    Initial commit

--- app/src/main/java/com/example/flupperapp/fragment_main_screen.kt
+++ app/src/main/java/com/example/flupperapp/fragment_main_screen.kt
@@ -5,18 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.flupperapp.adapters.MainTasksAdapter
+import com.example.flupperapp.adapters.TasksAdapter
 import com.example.flupperapp.databinding.FragmentMainBinding
 
 
-class Fragment_Main_Screen:Fragment(){
-    var _binding :FragmentMainBinding? = null
-    val binding get() = _binding ?: throw IllegalStateException ("null binding")
+class Fragment_Main_Screen : Fragment() {
+    var _binding: FragmentMainBinding? = null
+    val binding get() = _binding ?: throw IllegalStateException("null binding")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding =  FragmentMainBinding.inflate(inflater,container,false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return (binding.root)
     }
 
@@ -24,4 +27,6 @@ class Fragment_Main_Screen:Fragment(){
         super.onDestroyView()
         _binding = null
     }
+
 }
+
--- app/src/main/java/com/example/flupperapp/fragment_tasks.kt
+++ app/src/main/java/com/example/flupperapp/fragment_tasks.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.flupperapp.adapters.MainTasksAdapter
 import com.example.flupperapp.databinding.FragmentTasksBinding
 import com.example.flupperapp.adapters.TasksAdapter
 
-class FragmentTasks:Fragment(){
-    var _binding : FragmentTasksBinding? = null
-    val binding get()=_binding ?: throw IllegalStateException ("null binding")
-//    что принимает get в скобках
+class FragmentTasks : Fragment() {
+    var _binding: FragmentTasksBinding? = null
+    val binding get() = _binding ?: throw IllegalStateException("null binding")
+
+    //    что принимает get в скобках
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +23,7 @@ class FragmentTasks:Fragment(){
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -33,11 +36,25 @@ class FragmentTasks:Fragment(){
         val adapter = TasksAdapter(tasks)
 
         // Настраиваем RecyclerView как сетку
-        binding.recyclerView.apply {
+        binding.recyclerViewTasks.apply {
             this.adapter = adapter
 //                что такое контекст тут зачем он нужен
             layoutManager = LinearLayoutManager(context)
-    }
-}
 
+
+            val tasksMain = STUBER.mainTasksList
+// Создаём адаптер
+            val adapterMain = MainTasksAdapter(tasksMain)
+
+            // Настраиваем RecyclerView как сетку
+            //почему именно apply
+            binding.recyclerViewMainTasks.apply {
+                this.adapter = adapterMain
+//
+                layoutManager = LinearLayoutManager(context)
+
+            }
+        }
+
+    }
 }
